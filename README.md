df8138d6684fb27867bbf1878a63b99196e0de44
phcreery <phcreery@gmail.com>
2026-04-20 19:59:53 -0500

    add Makefile and fix deps

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Learning OpenGL by making a performance optimized Minecraft Voxel Clone
+
+Build dependencies:
+- glfw
+- glm
+- opengl32
--- shader.h
+++ shader.h
@@ -2,25 +2,25 @@
 #define SHADER_H
 #pragma once
 
-#include <glad/glad.h>
+#include "glad/glad.h"
 
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
 
-
 static unsigned int ID;
 
 class Shader
 {
 public:
-    Shader(const char* vertexPath, const char* fragmentPath);
+    Shader(const char *vertexPath, const char *fragmentPath);
     void use();
-    void setBool(const std::string& name, bool value) const;
-    void setInt(const std::string& name, int value) const;
-    void setFloat(const std::string& name, float value) const;
-    void setMat4(const std::string& name, const float* value) const;
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+    void setMat4(const std::string &name, const float *value) const;
+
 private:
     void checkCompileErrors(unsigned int shader, std::string type);
 };
